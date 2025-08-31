37383b1c6c7e0005e368ca554829d87ad03d52dd
Nicolas-Avila <aviladanielnicolas@gmail.com>
2025-08-31 14:24:45 -0300

    routerlink

--- tpTest/src/app/session/view/login/login.component.ts
+++ tpTest/src/app/session/view/login/login.component.ts
@@ -1,16 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { IonInput, IonItem, IonList, IonText } from '@ionic/angular/standalone';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { GlobalService } from '../../../core/services/global.service';
+import { IonInput, IonItem, IonList, IonText, IonButton, IonNav,IonContent } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports:[IonInput,IonText,IonItem, IonList]
+  standalone: true,
+  imports: [
+    IonInput,
+    IonItem,
+    IonList,
+    IonText,
+    IonButton,
+    ReactiveFormsModule,
+    IonNav,
+    IonContent,
+    RouterLink
+  ]
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  mensaje: string = '';
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder, private globalService: GlobalService, private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  async onLogin() {
+    if (this.loginForm.invalid) {
+      this.mensaje = 'Por favor completa todos los campos correctamente';
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+    const { data, error } = await this.globalService.signIn(email, password);
+
+    if (error) {
+      this.mensaje = 'Error: ' + error.message;
+    } else {
+      this.router.navigate(['/tabs/tab1']);
+      this.loginForm.reset();
+    }
+  }
 
 }
